rubinus/cilium

commit 0ecab37c49a37864599e07729825ba97bb9f8c14

wireguard: Remove operator
This commit removes the Wireguard operator which previously was used to
allocate a Wireguard tunnel IP.

After taking a closer look into the Wireguard source code in the kernel
and realising that we don't see the tunnel IP on the wire, we came to
realisation that the IP is not needed at all.

For host2host encryption with Wireguard (TODO) we don't need to rely on
the tunnel IP (we will be able to use the nodeIP and Wireguard's fwmark
to avoid the loop).

Signed-off-by: Martynas Pumputis <m@lambda.lt>

operator/flags.go
@@ -105,12 +105,6 @@ func init() {
 	flags.Bool(option.EnableWireguard, false, "Enable wireguard")
 	option.BindEnv(option.EnableWireguard)
 
-	flags.String(option.WireguardSubnetV4, defaults.WireguardSubnetV4, "Wireguard tunnel IPv4 subnet")
-	option.BindEnv(option.WireguardSubnetV4)
-
-	flags.String(option.WireguardSubnetV6, defaults.WireguardSubnetV6, "Wireguard tunnel IPv6 subnet")
-	option.BindEnv(option.WireguardSubnetV6)
-
 	var defaultIPAM string
 	switch binaryName {
 	case "cilium-operator":

daemon/cmd/daemon.go
@@ -531,7 +531,7 @@ func NewDaemon(ctx context.Context, cancel context.CancelFunc, epMgr *endpointma
 			return nil, restoredEndpoints, err
 		}
 
-		if option.Config.IPAM == ipamOption.IPAMClusterPool || option.Config.EnableWireguard {
+		if option.Config.IPAM == ipamOption.IPAMClusterPool {
 			// Create the CiliumNode custom resource. This call will block until
 			// the custom resource has been created
 			d.nodeDiscovery.UpdateCiliumNodeResource()

daemon/cmd/daemon_main.go
@@ -407,12 +407,6 @@ func init() {
 	flags.Bool(option.EnableWireguard, false, "Enable wireguard")
 	option.BindEnv(option.EnableWireguard)
 
-	flags.String(option.WireguardSubnetV4, defaults.WireguardSubnetV4, "Wireguard tunnel IPv4 subnet")
-	option.BindEnv(option.WireguardSubnetV4)
-
-	flags.String(option.WireguardSubnetV6, defaults.WireguardSubnetV6, "Wireguard tunnel IPv6 subnet")
-	option.BindEnv(option.WireguardSubnetV6)
-
 	flags.Bool(option.ForceLocalPolicyEvalAtSource, defaults.ForceLocalPolicyEvalAtSource, "Force policy evaluation of all local communication at the source endpoint")
 	option.BindEnv(option.ForceLocalPolicyEvalAtSource)
 
@@ -1555,8 +1549,7 @@ func runDaemon() {
 
 		var err error
 		privateKeyPath := filepath.Join(option.Config.StateDir, wireguardTypes.PrivKeyFilename)
-		wgAgent, err = wireguard.NewAgent(privateKeyPath,
-			option.Config.WireguardSubnetV4, option.Config.WireguardSubnetV6)
+		wgAgent, err = wireguard.NewAgent(privateKeyPath)
 		if err != nil {
 			log.WithError(err).Fatal("Failed to initialize wireguard")
 		}

operator/cilium_node.go
@@ -23,7 +23,6 @@ import (
 	"github.com/cilium/cilium/pkg/k8s/informer"
 	v1 "github.com/cilium/cilium/pkg/k8s/slim/k8s/apis/meta/v1"
 	"github.com/cilium/cilium/pkg/logging/logfields"
-	wireguard "github.com/cilium/cilium/pkg/wireguard/operator"
 
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"k8s.io/apimachinery/pkg/fields"
@@ -41,9 +40,7 @@ var (
 	k8sCiliumNodesCacheSynced = make(chan struct{})
 )
 
-func startSynchronizingCiliumNodes(nodeManager allocator.NodeEventHandler,
-	wgOperator *wireguard.Operator) {
-
+func startSynchronizingCiliumNodes(nodeManager allocator.NodeEventHandler) {
 	log.Info("Starting to synchronize CiliumNode custom resources")
 
 	// TODO: The operator is currently storing a full copy of the
@@ -60,11 +57,6 @@ func startSynchronizingCiliumNodes(nodeManager allocator.NodeEventHandler,
 				if node := k8s.ObjToCiliumNode(obj); node != nil {
 					// node is deep copied before it is stored in pkg/aws/eni
 					nodeManager.Create(node)
-					if wgOperator != nil {
-						if err := wgOperator.AddNode(node); err != nil {
-							log.WithError(err).Warn("Wireguard add node")
-						}
-					}
 				} else {
 					log.Warningf("Unknown CiliumNode object type %T received: %+v", obj, obj)
 				}
@@ -77,20 +69,12 @@ func startSynchronizingCiliumNodes(nodeManager allocator.NodeEventHandler,
 						}
 						// node is deep copied before it is stored in pkg/aws/eni
 						nodeManager.Update(node)
-						if wgOperator != nil {
-							if err := wgOperator.UpdateNode(node); err != nil {
-								log.WithError(err).Warn("Wireguard update node")
-							}
-						}
 					}
 				}
 			},
 			DeleteFunc: func(obj interface{}) {
 				if node := k8s.ObjToCiliumNode(obj); node != nil {
 					nodeManager.Delete(node.Name)
-					if wgOperator != nil {
-						wgOperator.DeleteNode(node)
-					}
 				}
 			},
 		},

operator/main.go
@@ -44,7 +44,6 @@ import (
 	"github.com/cilium/cilium/pkg/rand"
 	"github.com/cilium/cilium/pkg/rate"
 	"github.com/cilium/cilium/pkg/version"
-	wireguard "github.com/cilium/cilium/pkg/wireguard/operator"
 
 	gops "github.com/google/gops/agent"
 	"github.com/sirupsen/logrus"
@@ -364,19 +363,8 @@ func onOperatorStartLeading(ctx context.Context) {
 
 	var (
 		nodeManager *allocator.NodeEventHandler
-		wgOperator  *wireguard.Operator
 		err         error
 	)
-
-	if option.Config.EnableWireguard {
-		wgOperator, err = wireguard.NewOperator(
-			option.Config.WireguardSubnetV4,
-			option.Config.WireguardSubnetV6,
-			&ciliumNodeUpdateImplementation{})
-		if err != nil {
-			log.WithError(err).Fatal("Failed to create wireguard operator")
-		}
-	}
 
 	log.WithField(logfields.Mode, option.Config.IPAM).Info("Initializing IPAM")
 
@@ -396,7 +384,7 @@ func onOperatorStartLeading(ctx context.Context) {
 			log.WithError(err).Fatalf("Unable to start %s allocator", ipamMode)
 		}
 
-		startSynchronizingCiliumNodes(nm, wgOperator)
+		startSynchronizingCiliumNodes(nm)
 		nodeManager = &nm
 
 		switch ipamMode {
@@ -417,15 +405,8 @@ func onOperatorStartLeading(ctx context.Context) {
 			nm.Resync(context.Background(), time.Time{})
 		}
 	default:
-		startSynchronizingCiliumNodes(NOPNodeManager, wgOperator)
+		startSynchronizingCiliumNodes(NOPNodeManager)
 		nodeManager = &NOPNodeManager
-	}
-
-	if wgOperator != nil {
-		<-k8sCiliumNodesCacheSynced
-		if err := wgOperator.RestoreFinished(); err != nil {
-			log.WithError(err).Warn("Failed to allocate wireguard IP addrs")
-		}
 	}
 
 	if operatorOption.Config.BGPAnnounceLBIP {

pkg/datapath/linux/node.go
@@ -34,7 +34,6 @@ import (
 	"github.com/cilium/cilium/pkg/maps/neighborsmap"
 	"github.com/cilium/cilium/pkg/maps/tunnel"
 	"github.com/cilium/cilium/pkg/metrics"
-	"github.com/cilium/cilium/pkg/node/addressing"
 	nodeTypes "github.com/cilium/cilium/pkg/node/types"
 	"github.com/cilium/cilium/pkg/option"
 
@@ -926,8 +925,6 @@ func (n *linuxNodeHandler) nodeUpdate(oldNode, newNode *nodeTypes.Node, firstAdd
 
 	if option.Config.EnableWireguard {
 		var podCIDRv4, podCIDRv6 *net.IPNet
-		wgIPv4 := newNode.GetIPByType(addressing.NodeWireguardIP, false)
-		wgIPv6 := newNode.GetIPByType(addressing.NodeWireguardIP, true)
 		if newNode.IPv4AllocCIDR != nil {
 			podCIDRv4 = newNode.IPv4AllocCIDR.IPNet
 		}
@@ -935,7 +932,7 @@ func (n *linuxNodeHandler) nodeUpdate(oldNode, newNode *nodeTypes.Node, firstAdd
 			podCIDRv6 = newNode.IPv6AllocCIDR.IPNet
 		}
 		if err := n.wgAgent.UpdatePeer(newNode.Name, newNode.WireguardPubKey,
-			wgIPv4, newIP4, podCIDRv4, wgIPv6, newIP6, podCIDRv6); err != nil {
+			newIP4, podCIDRv4, newIP6, podCIDRv6); err != nil {
 			log.WithError(err).
 				WithField(logfields.NodeName, newNode.Name).
 				Warning("Failed to update wireguard configuration for peer")

pkg/datapath/wireguard.go
@@ -20,7 +20,7 @@ import "net"
 type WireguardAgent interface {
 	Init() error
 	UpdatePeer(nodeName, pubKeyHex string,
-		wgIPv4, nodeIPv4 net.IP, podCIDRv4 *net.IPNet,
-		wgIPv6, nodeIPv6 net.IP, podCIDRv6 *net.IPNet) error
+		nodeIPv4 net.IP, podCIDRv4 *net.IPNet,
+		nodeIPv6 net.IP, podCIDRv6 *net.IPNet) error
 	DeletePeer(nodeName string) error
 }

pkg/k8s/init.go
@@ -30,7 +30,6 @@ import (
 	k8sversion "github.com/cilium/cilium/pkg/k8s/version"
 	"github.com/cilium/cilium/pkg/logging/logfields"
 	"github.com/cilium/cilium/pkg/node"
-	"github.com/cilium/cilium/pkg/node/addressing"
 	nodeTypes "github.com/cilium/cilium/pkg/node/types"
 	"github.com/cilium/cilium/pkg/option"
 	"github.com/cilium/cilium/pkg/source"
@@ -121,19 +120,6 @@ func retrieveNodeInformation(nodeName string) (*nodeTypes.Node, error) {
 		// used to update state
 		n = ParseNode(typesNode, source.Unspec)
 		log.WithField(logfields.NodeName, n.Name).Info("Retrieved node information from kubernetes node")
-	}
-
-	if option.Config.EnableWireguard {
-		if option.Config.EnableIPv4 {
-			if ip := n.GetIPByType(addressing.NodeWireguardIP, false); ip == nil {
-				return nil, fmt.Errorf("wireguard IPv4 not available")
-			}
-		}
-		if option.Config.EnableIPv6 {
-			if ip := n.GetIPByType(addressing.NodeWireguardIP, true); ip == nil {
-				return nil, fmt.Errorf("wireguard IPv6 not available")
-			}
-		}
 	}
 
 	if requireIPv4CIDR && n.IPv4AllocCIDR == nil {
@@ -262,9 +248,6 @@ func WaitForNodeInformation() error {
 		if nodeIP6 != nil {
 			node.SetIPv6(nodeIP6)
 		}
-
-		node.SetWireguardIPv4(n.GetIPByType(addressing.NodeWireguardIP, false))
-		node.SetWireguardIPv6(n.GetIPByType(addressing.NodeWireguardIP, true))
 
 		node.SetLabels(n.Labels)
 

commit aa9a3cb4734d79f1e077015d39cbc8bcc35805a7

operator: Remove unused --enable-wireguard flag
The operator does not have any functionality that conditionally depends
on WireGuard anymore.

Fixes: 0ecab37c49a3 ("wireguard: Remove operator")

Signed-off-by: Sebastian Wicki <sebastian@isovalent.com>

operator/flags.go
@@ -91,9 +91,6 @@ func init() {
 			`configmap example for syslog driver: {"syslog.level":"info","syslog.facility":"local4"}`)
 	option.BindEnv(option.LogOpt)
 
-	flags.Bool(option.EnableWireguard, false, "Enable wireguard")
-	option.BindEnv(option.EnableWireguard)
-
 	var defaultIPAM string
 	switch binaryName {
 	case "cilium-operator":